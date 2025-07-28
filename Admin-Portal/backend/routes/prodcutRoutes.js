4a0003781f374c39d051297162d6f3c540b25c36
Priyankara2003 <srinathpriyankara77@gmail.com>
2025-07-28 11:34:24 +0530

    refactor : add middleware for authorize requests

--- Admin-Portal/backend/routes/prodcutRoutes.js
+++ Admin-Portal/backend/routes/prodcutRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { addProduct, deleteProduct, updateProduct, getAllProduct, getProductById } from '../controllers/productController.js';
+import authenticate from '../middlewares/authMiddleware.js';
 
 const router=express.Router();
 
-router.post('/add', addProduct);
-router.post('/delete/:id', deleteProduct);
-router.post('/update/:id', updateProduct);
-router.get('/getAll/:id', getAllProduct);
-router.get('/get/:id', getProductById);
+router.post('/add', authenticate, addProduct);
+router.post('/delete/:id', authenticate, deleteProduct);
+router.post('/update/:id', authenticate, updateProduct);
+router.get('/getAll/:id', authenticate, getAllProduct);
+router.get('/get/:id', authenticate, getProductById);
 
 export default router;
