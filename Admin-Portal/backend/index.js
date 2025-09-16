8fda185abbfee42767b8d867170160bed6176127
Priyankara2003 <srinathpriyankara77@gmail.com>
2025-09-16 23:43:33 +0530

    refactor: update CORS origin to new IP address for backend access

--- Admin-Portal/backend/index.js
+++ Admin-Portal/backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      origin: ["http://localhost:3000","http://3.90.28.145:3000"],
+      origin: ["http://localhost:3000","http://47.129.174.20:3000"],
       credentials: true,
     })
   );
